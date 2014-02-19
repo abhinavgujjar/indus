ae800af2dcd85ceaba0a8e00f15e3d6a3cc1203f
abhinav <abhinavgujjar@gmail.com>
2014-02-19 17:32:21 +0530

    hotels are there now

---
+++ angular-seed-master/angular-seed-master/app/js/controllers/hotelsController.js
@@ -0,0 +1,24 @@
+myApp.controller('MyCtrl1', function($scope) {
+
+	$scope.hotels = [
+	{
+		name : 'Lalit Ashok',
+		location : 'Delhi',
+		rating: 5,
+		image: 'img/ashok.jpg'
+	},
+	{
+		name: 'Hyatt Regency',
+		lcoation: 'Mumbai',
+		rating: 10,
+		image: 'img/taj.jpg'
+	},
+	{
+		name: 'Taj Residency',
+		location: 'New Delhi',
+		rating : 3,
+		image: 'img/taj.jpg'
+	}
+	]
+}
+);
