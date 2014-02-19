831d7632859dfc403323dd139832a1947098eb39
abhinav <abhinavgujjar@gmail.com>
2014-02-19 18:33:36 +0530

    filters added in

--- angular-seed-master/angular-seed-master/app/js/controllers/hotelsController.js
+++ angular-seed-master/angular-seed-master/app/js/controllers/hotelsController.js
@@ -3,21 +3,43 @@ myApp.controller('MyCtrl1', function($scope) {
 	$scope.hotels = [
 	{
 		name : 'Lalit Ashok',
-		location : 'Delhi',
+		location : '<i>Delhi</i><script>alert("BOO");</script>',
 		rating: 5,
-		image: 'img/ashok.jpg'
+		image: 'img/ashok.jpg',
+		reviews: [{
+			author: 'abhinav',
+			comment: 'good one'
+		}]
 	},
 	{
 		name: 'Hyatt Regency',
 		lcoation: 'Mumbai',
 		rating: 10,
-		image: 'img/taj.jpg'
+		image: 'img/hyatt.jpg'
 	},
 	{
 		name: 'Taj Residency',
 		location: 'New Delhi',
 		rating : 3,
 		image: 'img/taj.jpg'
+	},
+	{
+		name: 'Country Inn',
+		lcoation: 'Dubai',
+		rating: 7,
+		image: 'img/countryinn.jpg'
+	},
+	{
+		name: 'Royal Orchid',
+		lcoation: 'Bangalore',
+		rating: 2,
+		image: 'img/royalorchid.jpg'
+	},
+	{
+		name: 'Radisson',
+		lcoation: 'Mumbai',
+		rating: 3,
+		image: 'img/radisson.jpg'
 	}
 	]
 }
