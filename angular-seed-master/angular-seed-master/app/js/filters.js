088a49393e3a61fe7681fd87cb8762f06fbbb8d4
abhinav <abhinavgujjar@gmail.com>
2014-02-20 12:46:17 +0530

    filters

--- angular-seed-master/angular-seed-master/app/js/filters.js
+++ angular-seed-master/angular-seed-master/app/js/filters.js
@@ -8,3 +8,23 @@ angular.module('myApp.filters', []).
       return String(text).replace(/\%VERSION\%/mg, version);
     }
   }]);
+
+  myApp.filter('toRupees', function (){
+  	return function(dollarRate){
+  		return dollarRate * 60;
+  	}
+  });
+
+  myApp.filter('categories', function (){
+  	return function(rate){
+  		if (rate > 100000)
+  			return 'Luxury';
+		if (rate > 50000)
+  			return 'Premium';
+  		if (rate > 20000)
+  			return 'Regular';
+  		
+  		return 'Lodge';
+  	}
+  });
+
